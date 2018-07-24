197cbbfb7bc49b8512023ff8fdedc6d1d3a1c4f5
SauravMahata2401 <sauravmahata24@gmail.com>
2018-07-25 00:40:04 +0530

    commit2

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8286059e-3dd3-4c8f-be8a-653a3f17fc22</testSuiteGuid>
+</TestSuiteEntity>
